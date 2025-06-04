1b7a3aaf60e3de37df9ab6f8d4f314e8f2e371f4
Your Name <you@example.com>
2025-06-04 18:56:19 +0300

    Fix errs

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -2,19 +2,18 @@ import { getProducts } from "@/helpers";
 import React from "react";
 import Container from "./Container";
 import ProductsData from "./ProductsData";
-import { Products } from "../../type";
+import { Products as ProductType } from "../../type";
 
-
-const Products = async () => {
-  const products = await getProducts();
+const ProductsList = async () => {
+  const products: ProductType[] = await getProducts();
 
   return (
     <Container className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-4 -mt-10">
-      {products?.map((item:Products) => (
+      {products?.map((item) => (
         <ProductsData item={item} key={item._id} />
       ))}
     </Container>
   );
 };
 
-export default Products;
+export default ProductsList;
